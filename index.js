c10b15547424d3871c9f523bffba3b652f3600c3
N0rule <N0rule228@gmail.com>
2024-03-27 14:46:20 +0200

    timed commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('module-alias/register')
-const { Client } = require('node-osc');
+const { Bundle, Client } = require('node-osc');
 
 const http = require('http');
 
-const { hostname, port} = require("@root/config.js");
+const { chatbox, hostname, port} = require("@root/config.js");
 
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
@@ -20,10 +20,15 @@ const server = http.createServer((req, res) => {
       // VRCHAT OSC client
       const vrchatOSC = new Client('127.0.0.1', 9000);
       const heartRateValue = parseInt(hr, 10);
-      vrchatOSC.send('/avatar/parameters/Heartrate', heartRateValue, () => {
-      //console.log('Heart rate sent to VRChat OSC');
-      vrchatOSC.close();
-      });
+      if (chatbox === true) {
+        const vrchatOSCTextBox = new Bundle(['/avatar/parameters/Heartrate', heartRateValue], ['/chatbox/input', `❤ ${heartRateValue}`]);
+        vrchatOSC.send(vrchatOSCTextBox);
+      } else {
+        vrchatOSC.send('/chatbox/input', `❤ ${heartRateValue}`,() => {
+          //console.log('Heart rate sent to VRChat OSC');
+          vrchatOSC.close();
+        });
+      }
       // Send response
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
