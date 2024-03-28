daeea7fc4d8eab61f27bd22944be39b3abf7878d
N0rule <N0rule228@gmail.com>
2024-03-28 10:58:36 +0200

    forgot config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    hostname: '192.168.0.100', // your local IP 
+    port: 6547, // Port that your watch app is running on
+    chatbox: true, // Shoud Heart Rate be displayed in chatbox
+    chatbox_message: "❤ {heartRate}" //custom message for chatbox, use {heartRate} for Heart Rate
+};
+
+
+
+
